6d88fde434e5df5b55eae777526ff148be00f14d
MalcolmScruggs <mookie.dev@gmail.com>
2019-10-21 13:12:30 -0400

    Change to alert

--- src/App.js
+++ src/App.js
@@ -141,42 +141,8 @@ function TabContent(props) {
 function VaccinationModal(props) {
     return (
         <div>
-            <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+            <div type="button" className="btn btn-primary" onClick={() => alert("Not supported at this time")}>
                 Add Vaccination
-            </button>
-
-            <div className="modal fade" id="exampleModal" tabIndex="-1" role="dialog"
-                 aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                        <div className="modal-header">
-                            <h5 className="modal-title" id="exampleModalLabel">Add Vaccine</h5>
-                            <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div className="modal-body">
-                            <div className="input-group input-group-sm mb-3">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text" id="inputGroup-sizing-sm">Vaccine</span>
-                                </div>
-                                <input type="text" className="form-control" aria-label="Small"
-                                       aria-describedby="inputGroup-sizing-sm"/>
-                            </div>
-                            <div className="input-group input-group-sm mb-3">
-                                <div className="input-group-prepend">
-                                    <span className="input-group-text" id="inputGroup-sizing-sm">Last Given</span>
-                                </div>
-                                <input type="text" className="form-control" aria-label="Small"
-                                       aria-describedby="inputGroup-sizing-sm"/>
-                            </div>
-                        </div>
-                        <div className="modal-footer">
-                            <button type="button" className="btn btn-secondary" data-dismiss="modal">Close</button>
-                            <button type="button" className="btn btn-primary">Save changes</button>
-                        </div>
-                    </div>
-                </div>
             </div>
         </div>
     )
